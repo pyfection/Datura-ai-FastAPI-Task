c0570380c5b021d8f7937a58435df7d19e2c8d6c
pyfection <mat@pyfection.com>
2025-03-21 18:02:36 -0300

    Add function to get a sentiment value between -100 and 100 from a string by using an LLM.

--- api/stake_extrinsic_sentiment.py
+++ api/stake_extrinsic_sentiment.py
@@ -1,10 +1,15 @@
+import json
 import os
 from datetime import date, timedelta
 
 import requests
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATURA_API_KEY = os.environ.get("DATURA_API_KEY")
+CHUTES_API_KEY = os.environ.get("CHUTES_API_KEY")
 
 
 def fetch_bittensor_netuid_tweets(netuid: int) -> list[dict]:
@@ -31,3 +36,41 @@ def fetch_bittensor_netuid_tweets(netuid: int) -> list[dict]:
 
     print(response.status_code)  # ToDo: raise proper error message if not 200
     return response.json()
+
+
+def fetch_chutes_sentiment(text: str) -> int:
+    """Return a sentiment based on the given text between -100 (very bad) and 100 (very good)."""
+    base = "Return a sentiment score between -100 and +100 (only the score and nothing else) for the following data:"
+    url = "https://llm.chutes.ai/v1/chat/completions"
+    headers = {
+        "Authorization": f"BEARER {CHUTES_API_KEY}",
+        "Content-Type": "application/json",
+    }
+    payload = {
+        "model": "unsloth/Llama-3.2-3B-Instruct",
+        "messages": [{"role": "user", "content": f"{base}\n{text}"}],
+        "stream": True,
+        "max_tokens": 1024,
+        "temperature": 0.5,
+    }
+    response = requests.post(url, json=payload, headers=headers)
+
+    # Note: response doesn't give a JSON for some reason, which is why I put this workaround.
+    lines = response.text.split("data: ")
+    content = []
+    for line in lines:
+        line = line.strip()
+        if not line or line == "[DONE]":
+            continue
+        data = json.loads(line)
+        choices = data["choices"]
+        content.append("".join([c["delta"]["content"] for c in choices]))
+    return int("".join(content))
+
+
+if __name__ == "__main__":
+    # tweets = fetch_bittensor_netuid_tweets(18)
+    # for tweet in tweets:
+    #     print(tweet["text"])
+    #     break
+    print(fetch_chutes_sentiment("I'm OK today"))
